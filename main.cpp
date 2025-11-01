d86441bfcc7ff7b151f51c73354d8b012a58e420
BILLIEN Clement <clement.billien@sacem.fr>
2025-11-01 17:07:03 +0100

    update image.cpp and image.hpp to use the Color class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <tuple>
+#include "raymath/Color.hpp"
 #include "image/Image.hpp"
 
 int main()
@@ -9,14 +9,16 @@ int main()
 
     Image image(image_width, image_height);
 
+    // Dégradé horizontal du rouge (à gauche) vers le bleu (à droite)
     for (int j = 0; j < image_height; ++j) {
         for (int i = 0; i < image_width; ++i) {
-            auto r = static_cast<float>(i) / (image_width - 1);
-            auto g = static_cast<float>(j) / (image_height - 1);
-            auto b = 0.2f;
+            float t = float(i) / (image_width - 1); // 0 à 1
 
-            ColorTuple pixel_color = std::make_tuple(r, g, b);
+            float r = 1.0f * (1.0f - t); 
+            float g = 0.0f;              
+            float b = 1.0f * t;          
 
+            Color pixel_color(r, g, b);
             image.SetPixel(i, j, pixel_color);
         }
     }
