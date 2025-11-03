289a09c4fd9fb67ad2772e1d82078e5ead9b8930
Jean Cazals <jcdfabel@pragmatik.fr>
2025-11-03 15:00:23 +0100

    Add Hittable interface and hit_record structure
    - Introduced Hittable class as an abstract interface for objects that can be hit by rays.
    - Added hit_record structure to store information about ray-object intersections.
    - Implemented set_face_normal method to determine the orientation of the hit normal based on the incoming ray.

---
+++ include/raymath/Hittable.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Vector.hpp"
+#include "Ray.hpp"
+
+struct hit_record {
+    Vector point;
+    Vector normal;
+    float t;
+    bool front_face;
+    
+    void set_face_normal(const Ray& ray, const Vector& outward_normal);
+};
+
+class Hittable {
+public:
+    virtual ~Hittable() = default;
+    
+    virtual bool hit(const Ray& ray,
+                     float t_min,
+                     float t_max,
+                     hit_record& rec) const = 0;
+};
+
