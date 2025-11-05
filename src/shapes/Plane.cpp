76d2bb23a181cf2ca59059a83da41de5a92e32f9
Doud75 <adrienquimbre@gmail.com>
2025-11-05 08:57:22 +0100

    RAY-70 : fix plan

--- src/shapes/Plane.cpp
+++ src/shapes/Plane.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 
 Plane::Plane(float y, float scale) 
-    : y_position(y), checker_scale(scale) {}
+    : y_position(y) {}
 
 bool Plane::hit(const Ray& ray, 
                 float t_min, 
@@ -27,19 +27,6 @@ bool Plane::hit(const Ray& ray,
     const Vector outward_normal(0.0f, 1.0f, 0.0f);
     rec.set_face_normal(ray, outward_normal);
 
-    if (isWhiteSquare(rec.point.X(), rec.point.Z())) {
-        rec.color = Color(0.9f, 0.9f, 0.9f);
-    } else {
-        rec.color = Color(0.2f, 0.2f, 0.2f);
-    }
-
     return true;
-}
-
-bool Plane::isWhiteSquare(float x, float z) const {
-    const int xi = static_cast<int>(std::floor(x / checker_scale));
-    const int zi = static_cast<int>(std::floor(z / checker_scale));
-    
-    return ((xi + zi) % 2) == 0;
 }
 
