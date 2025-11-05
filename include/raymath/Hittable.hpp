79e36746b1563c9cdec22dff57f8e07c78a775cc
cbillien <billienclement@hotmail.com>
2025-11-05 10:00:29 +0100

    intégration material à hittable

--- include/raymath/Hittable.hpp
+++ include/raymath/Hittable.hpp
@@ -4,13 +4,17 @@
 #include "Ray.hpp"
 #include "Color.hpp"
 
+class Material; 
+
 struct hit_record {
     Vector point;
     Vector normal;
     float t;
     bool front_face;
-    Color color;
+    std::shared_ptr<Material> mat; // pointeur vers le materiau (plus besoin de définir la couleur ici
+    //  chaque matériau aura ça couleur grace à scatter
     
+
     void set_face_normal(const Ray& ray, const Vector& outward_normal);
 };
 
